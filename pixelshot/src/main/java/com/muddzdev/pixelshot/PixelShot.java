9fe7f36215a5a4159e9af516594729439087471d
David Fraska <dfraska@ftllabscorp.com>
2019-04-18 13:25:36 -0400

    Support for non-external storage locations

--- pixelshot/src/main/java/com/muddzdev/pixelshot/PixelShot.java
+++ pixelshot/src/main/java/com/muddzdev/pixelshot/PixelShot.java
@@ -17,6 +17,7 @@ import android.view.View;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.ref.WeakReference;
 
@@ -47,10 +48,11 @@ public class PixelShot {
     private static final String EXTENSION_NOMEDIA = ".nomedia";
     private static final int JPG_MAX_QUALITY = 100;
 
-    private String path = Environment.DIRECTORY_PICTURES;
+    private File path = new File(Environment.getExternalStorageDirectory(), Environment.DIRECTORY_PICTURES);
     private String filename = String.valueOf(System.currentTimeMillis());
     private String fileExtension = EXTENSION_JPG;
     private int jpgQuality = JPG_MAX_QUALITY;
+    private boolean isExternal = true;
 
     private PixelShotListener listener;
     private View view;
@@ -68,9 +70,28 @@ public class PixelShot {
         return this;
     }
 
-    public PixelShot setPath(String path) {
+    public PixelShot setPath(File path, boolean isExternal) {
         this.path = path;
+        this.isExternal = isExternal;
         return this;
+    }
+
+    public PixelShot setPath(File path) throws IOException {
+        return setPath(path, path.getCanonicalPath().contains(
+                Environment.getExternalStorageDirectory().getCanonicalPath() + File.separator));
+    }
+
+    public PixelShot setPath(String path, boolean relativeToExternal, boolean isExternal) {
+        this.isExternal = isExternal;
+        if (relativeToExternal) {
+            return setPath(new File(Environment.getExternalStorageDirectory(), path), isExternal);
+        } else {
+            return setPath(new File(path), isExternal);
+        }
+    }
+
+    public PixelShot setPath(String path) {
+        return setPath(path, true, true);
     }
 
     public PixelShot setResultListener(PixelShotListener listener) {
@@ -105,18 +126,20 @@ public class PixelShot {
      */
 
     public void save() throws NullPointerException {
-        if (!Utils.isStorageReady()) {
-            throw new IllegalStateException("Storage was not ready for use");
-        }
-        if (!Utils.isPermissionGranted(getAppContext())) {
-            throw new SecurityException("Permission WRITE_EXTERNAL_STORAGE is missing");
+        if (isExternal) {
+            if (!Utils.isStorageReady()) {
+                throw new IllegalStateException("Storage was not ready for use");
+            }
+            if (!Utils.isPermissionGranted(getAppContext())) {
+                throw new SecurityException("Permission WRITE_EXTERNAL_STORAGE is missing");
+            }
         }
 
         if (view instanceof SurfaceView) {
             PixelCopyHelper.getSurfaceBitmap((SurfaceView) view, new PixelCopyHelper.PixelCopyListener() {
                 @Override
                 public void onSurfaceBitmapReady(Bitmap surfaceBitmap) {
-                    new BitmapSaver(getAppContext(), surfaceBitmap, path, filename, fileExtension, jpgQuality, listener).execute();
+                    new BitmapSaver(getAppContext(), surfaceBitmap, path, filename, fileExtension, jpgQuality, isExternal, listener).execute();
                 }
 
                 @Override
@@ -128,7 +151,7 @@ public class PixelShot {
                 }
             });
         } else {
-            new BitmapSaver(getAppContext(), getViewBitmap(), path, filename, fileExtension, jpgQuality, listener).execute();
+            new BitmapSaver(getAppContext(), getViewBitmap(), path, filename, fileExtension, jpgQuality, isExternal, listener).execute();
         }
     }
 
@@ -175,20 +198,23 @@ public class PixelShot {
         private final WeakReference<Context> weakContext;
         private Handler handler = new Handler(Looper.getMainLooper());
         private Bitmap bitmap;
-        private String path;
+        private File path;
         private String filename;
         private String fileExtension;
         private int jpgQuality;
         private PixelShotListener listener;
         private File file;
+        private boolean runMediaScanner;
 
-        BitmapSaver(Context context, Bitmap bitmap, String path, String filename, String fileExtension, int jpgQuality, PixelShotListener listener) {
+        BitmapSaver(Context context, Bitmap bitmap, File path, String filename, String fileExtension, int jpgQuality,
+                    boolean runMediaScanner, PixelShotListener listener) {
             this.weakContext = new WeakReference<>(context);
             this.bitmap = bitmap;
             this.path = path;
             this.filename = filename;
             this.fileExtension = fileExtension;
             this.jpgQuality = jpgQuality;
+            this.runMediaScanner = runMediaScanner;
             this.listener = listener;
         }
 
@@ -206,13 +232,12 @@ public class PixelShot {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            File directory = new File(Environment.getExternalStorageDirectory(), path);
-            if (!directory.exists() && !directory.mkdirs()) {
+            if (!path.exists() && !path.mkdirs()) {
                 cancelTask();
                 return null;
             }
 
-            file = new File(directory, filename + fileExtension);
+            file = new File(path, filename + fileExtension);
             try (OutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
                 switch (fileExtension) {
                     case EXTENSION_JPG:
@@ -235,7 +260,11 @@ public class PixelShot {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            MediaScannerConnection.scanFile(weakContext.get(), new String[]{file.getPath()}, null, this);
+            if (runMediaScanner) {
+                MediaScannerConnection.scanFile(weakContext.get(), new String[]{file.getPath()}, null, this);
+            } else {
+                listener.onPixelShotSuccess(file.getPath());
+            }
             weakContext.clear();
         }
 
